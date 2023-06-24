e4c78f66780dbd9feddf8803829ac1dc77b740d5
Dapyu <113735974+Dapyu@users.noreply.github.com>
2023-06-23 22:06:05 -0300

    Add files via upload

---
+++ Unidad 1/Apuntes en practica/Calculos entre números.py
@@ -0,0 +1,7 @@
+1 + 1   # suma
+1 - 1   # resta
+1 * 1   # multiplicar
+2 ** 3  # La potencia 
+10 // 5 # división entera
+10 / 5  # división con decimal
+11 % 2  # resto de la división
---
+++ Unidad 1/Apuntes en practica/Definiciones.py
@@ -0,0 +1,23 @@
+# Definiciones
+
+''' Algoritmo: es una secuencia de pasos bien definida que deben realizarse en forma
+secuencial para lograr un resultado. No debe dejar lugar a dudas sobre el significado de
+cada paso (no ambigua). 
+
+
+Lenguaje de programación: es un lenguaje formal que puede ser utilizado por un
+programador para dar instrucciones a una computadora con el fin de controlar el
+comportamiento físico/lógico para obtener algún resultado. 
+
+● Compilado: se escriben las instrucciones (programa) y luego un programa
+especial llamado compilador las traduce todas juntas a otro lenguaje llamado
+de maquina que entiende el microprocesador. Ejemplo: Python
+
+● Interpretado: las instrucciones se leen y ejecutan una por una a medida que se
+ingresan. Podemos escribir todo el programa y luego dárselo al intérprete, esto
+se llama escribir un guion. 
+
+Sentencia: cada instrucción que puede ejecutarse dentro de un programa. 
+
+Expresión: un conjunto de valores y operandos que siempre resuelve a un resultado'''
+
